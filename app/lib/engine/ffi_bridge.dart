d59fe84935bc85135d9acb7ccaf8e786882e553c
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-21 22:29:54 +0700

    Update ffi_bridge.dart

--- app/lib/engine/ffi_bridge.dart
+++ app/lib/engine/ffi_bridge.dart
@@ -1,42 +1,31 @@
 import 'dart:ffi';
 import 'dart:io';
-import 'package:ffi/ffi.dart'; // ini yg penting
-
-// C function signatures
-typedef NativeAddC = Int32 Function(Int32 a, Int32 b);
-typedef NativeAddDart = int Function(int a, int b);
+import 'dart:convert';
+import 'package:ffi/ffi.dart';
 
 typedef StartFeedC = Void Function();
 typedef StartFeedDart = void Function();
 
-typedef GetLastTickC = Pointer<Utf8> Function();
-typedef GetLastTickDart = Pointer<Utf8> Function();
+typedef GetAllTicksC = Pointer<Utf8> Function();
+typedef GetAllTicksDart = Pointer<Utf8> Function();
 
 class BeiEngine {
-  late NativeAddDart nativeAdd;
-  late StartFeedDart startFeed;
-  late GetLastTickDart _getLastTick;
-  late DynamicLibrary _lib;
+  late final DynamicLibrary _lib;
+  late final StartFeedDart startFeed;
+  late final GetAllTicksDart _getAllTicks;
 
   BeiEngine() {
-    if (Platform.isAndroid) {
-      _lib = DynamicLibrary.open('libbei_engine.so');
-    } else if (Platform.isIOS) {
-      _lib = DynamicLibrary.process();
-    } else {
-      throw UnsupportedError('Platform not supported');
-    }
-    
-    nativeAdd = _lib.lookupFunction<NativeAddC, NativeAddDart>('native_add');
+    _lib = Platform.isAndroid
+       ? DynamicLibrary.open('libbei_engine.so')
+        : DynamicLibrary.process();
     startFeed = _lib.lookupFunction<StartFeedC, StartFeedDart>('start_feed');
-    _getLastTick = _lib.lookupFunction<GetLastTickC, GetLastTickDart>('get_last_tick');
+    _getAllTicks = _lib.lookupFunction<GetAllTicksC, GetAllTicksDart>('get_all_ticks');
   }
 
-  String getLastTick() {
-    final ptr = _getLastTick();
-    if (ptr == nullptr) return '';
-    final result = ptr.toDartString(); // toDartString() udah dari package:ffi
-    // calloc.free(ptr); // HANYA kalau C++ kamu ga free sendiri. Hati2 double free
-    return result;
+  Map<String, String> getAllTicks() {
+    final resultPtr = _getAllTicks();
+    final jsonStr = resultPtr.toDartString();
+    final Map<String, dynamic> decoded = jsonDecode(jsonStr);
+    return decoded.map((k, v) => MapEntry(k, v.toString()));
   }
 }
