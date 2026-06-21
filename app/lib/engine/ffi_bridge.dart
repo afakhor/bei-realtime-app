ff84f9107d625a17e00cf0d5df1fe8c7d4f52753
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-21 22:41:16 +0700

    Update ffi_bridge.dart

--- app/lib/engine/ffi_bridge.dart
+++ app/lib/engine/ffi_bridge.dart
@@ -5,27 +5,35 @@ import 'package:ffi/ffi.dart';
 
 typedef StartFeedC = Void Function();
 typedef StartFeedDart = void Function();
-
-typedef GetAllTicksC = Pointer<Utf8> Function();
-typedef GetAllTicksDart = Pointer<Utf8> Function();
+typedef GetAllDataC = Pointer<Utf8> Function();
+typedef GetAllDataDart = Pointer<Utf8> Function();
+typedef GetCandlesC = Pointer<Utf8> Function(Pointer<Utf8>);
+typedef GetCandlesDart = Pointer<Utf8> Function(Pointer<Utf8>);
 
 class BeiEngine {
   late final DynamicLibrary _lib;
   late final StartFeedDart startFeed;
-  late final GetAllTicksDart _getAllTicks;
+  late final GetAllDataDart _getAllData;
+  late final GetCandlesDart _getCandles;
 
   BeiEngine() {
-    _lib = Platform.isAndroid
-       ? DynamicLibrary.open('libbei_engine.so')
-        : DynamicLibrary.process();
+    _lib = Platform.isAndroid? DynamicLibrary.open('libbei_engine.so') : DynamicLibrary.process();
     startFeed = _lib.lookupFunction<StartFeedC, StartFeedDart>('start_feed');
-    _getAllTicks = _lib.lookupFunction<GetAllTicksC, GetAllTicksDart>('get_all_ticks');
+    _getAllData = _lib.lookupFunction<GetAllDataC, GetAllDataDart>('get_all_data');
+    _getCandles = _lib.lookupFunction<GetCandlesC, GetCandlesDart>('get_candles');
   }
 
-  Map<String, String> getAllTicks() {
-    final resultPtr = _getAllTicks();
-    final jsonStr = resultPtr.toDartString();
-    final Map<String, dynamic> decoded = jsonDecode(jsonStr);
-    return decoded.map((k, v) => MapEntry(k, v.toString()));
+  Map<String, dynamic> getAllData() {
+    final ptr = _getAllData();
+    final jsonStr = ptr.toDartString();
+    return jsonDecode(jsonStr);
+  }
+
+  List<dynamic> getCandles(String code) {
+    final codePtr = code.toNativeUtf8();
+    final ptr = _getCandles(codePtr);
+    final jsonStr = ptr.toDartString();
+    malloc.free(codePtr);
+    return jsonDecode(jsonStr);
   }
 }
