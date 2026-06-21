da15a77353262f3ae253256f4f73cfc72c5ff2d3
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-21 21:00:31 +0700

    Update ffi_bridge.dart

--- app/lib/engine/ffi_bridge.dart
+++ app/lib/engine/ffi_bridge.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'dart:io';
+import 'package:ffi/ffi.dart'; // ini yg penting
 
 // C function signatures
 typedef NativeAddC = Int32 Function(Int32 a, Int32 b);
@@ -15,32 +16,27 @@ class BeiEngine {
   late NativeAddDart nativeAdd;
   late StartFeedDart startFeed;
   late GetLastTickDart _getLastTick;
+  late DynamicLibrary _lib;
 
   BeiEngine() {
-    final lib = DynamicLibrary.open('libbei_engine.so');
-    nativeAdd = lib.lookupFunction<NativeAddC, NativeAddDart>('native_add');
-    startFeed = lib.lookupFunction<StartFeedC, StartFeedDart>('start_feed');
-    _getLastTick = lib.lookupFunction<GetLastTickC, GetLastTickDart>('get_last_tick');
+    if (Platform.isAndroid) {
+      _lib = DynamicLibrary.open('libbei_engine.so');
+    } else if (Platform.isIOS) {
+      _lib = DynamicLibrary.process();
+    } else {
+      throw UnsupportedError('Platform not supported');
+    }
+    
+    nativeAdd = _lib.lookupFunction<NativeAddC, NativeAddDart>('native_add');
+    startFeed = _lib.lookupFunction<StartFeedC, StartFeedDart>('start_feed');
+    _getLastTick = _lib.lookupFunction<GetLastTickC, GetLastTickDart>('get_last_tick');
   }
 
   String getLastTick() {
     final ptr = _getLastTick();
-    return ptr.toDartString();
-  }
-}
-
-// Helper buat convert Pointer<Utf8>
-class Utf8 extends Struct {}
-extension Utf8Pointer on Pointer<Utf8> {
-  String toDartString() {
-    final codeUnits = <int>[];
-    var i = 0;
-    while (true) {
-      final char = cast<Uint8>().elementAt(i).value;
-      if (char == 0) break;
-      codeUnits.add(char);
-      i++;
-    }
-    return String.fromCharCodes(codeUnits);
+    if (ptr == nullptr) return '';
+    final result = ptr.toDartString(); // toDartString() udah dari package:ffi
+    // calloc.free(ptr); // HANYA kalau C++ kamu ga free sendiri. Hati2 double free
+    return result;
   }
 }
